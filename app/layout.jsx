e978c6eb94a043260919537dd881f01c9c4db736
wifey143 <wifey121011@gmail.com>
2025-12-25 09:36:29 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,15 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Christmas Gift 🎄",
-  description: "A special Christmas website"
+  description: "A special Christmas website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>
+      <body>
+        <div className="snow"></div>
         {children}
       </body>
     </html>
