dc13e8acdd5c258191f97e2b3555ef6ce8eff49a
wifey143 <wifey121011@gmail.com>
2025-12-25 13:24:22 +0530

    Update page.jsx

--- app/message/page.jsx
+++ app/message/page.jsx
@@ -1,3 +1,4 @@
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap" rel="stylesheet">
 "use client";
 import { useRouter } from "next/navigation";
 
@@ -6,7 +7,7 @@ export default function Page3() {
 
   return (
     <main style={styles.main}>
-      <h1 style={styles.name}>Miss Harshita ✨️</h1>
+      <h1 style="font-family: 'Cinzel', serif;">Miss Harshita ✨</h1>
 
       <div style={styles.card}>
         <p>
